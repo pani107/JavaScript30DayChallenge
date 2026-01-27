1c6d1866878d4085bcd8df9ba88fa7701548e4bb
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-01-27 18:58:10 +0530

    Time: 49 ms (86.86%) | Memory: 55.1 MB (68.00%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        let result = x;
+
+        // apply functions from right to left
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    };
+};
